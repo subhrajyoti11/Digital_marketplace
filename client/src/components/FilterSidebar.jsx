b33dba15a3836b41b03a27b7094bbc693d22f7d0
Akash-Behera-cmd <akashbehera955649@gmail.com>
2026-06-23 09:35:51 +0530

    marketplace is ongoing

---
+++ client/src/components/FilterSidebar.jsx
@@ -0,0 +1,36 @@
+import { Filter, X } from 'lucide-react'
+import React, { useState } from 'react'
+
+const FilterSidebar = ({showFilterPhone,setShowFilterPhone,filters,setFilters}) => {
+    
+    
+    const [search,setSearch]=useState("")
+  return (
+        <div className={` ${showFilterPhone ? "max-sm:fixed" : "max-sm:hidden"} max-sm:inset-0 max-sm:w-full z-50 max-sm:h-screen max-sm:overflow-scroll bg-white rounded-lg shadow-sm border-gray-200 h-fit sticky top-24 min-w-[300px]`}>
+             <div className='p-4 border-b border border-gray-200'>
+                 <div className='flex items-center justify-between'>
+                     <div className='flex items-center space-x-2 text-gray-700'>
+                        <Filter className='size-4'/>
+                        <h3 className='font-semibold'>Filters</h3>
+                     </div>
+                     <div className='flex items-center gap-2'>
+
+                          <X className='size-6 text-gray-500 hover:text-gray-700 p-1 hover:bg-gray-100 rounded transition-colors cursor-pointer'/>
+
+                          <button onClick={()=>setShowFilterPhone(false)} className='sm:hidden text-sm border text-gray-700 px-3 py-1 rounded'>Apply</button>
+                     </div>
+                 </div>
+             </div>
+
+             <div className='p-4 space-y-6 sm:max-h-[calc(100vh-200px)] overflow-y-scroll no-scrollbar'>
+                
+                {/* Search Bar */}
+                <div className='flex items-center justify-between'>
+                   <input type="text" placeholder='Search by username,platform,niche,etc.' className='w-full text-sm px-3 py-2 border border-gray-300 rounded-md outline-indigo-500'/>
+                </div>
+             </div>
+        </div>
+  )
+}
+
+export default FilterSidebar
--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { ArrowLeftIcon, FilterIcon, Verified } from 'lucide-react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import ListingCard from '../components/ListingCard'
+import FilterSidebar from '../components/FilterSidebar'
 
 const Marketplace = () => {
+  
+  const navigate = useNavigate()
+
+  const [showFilterPhone,setShowFilterPhone] = useState(false)
+  const [filters,setFilters]=useState({
+    platform:null,
+    maxPrice: 100000,
+    minFollowers:0,
+    niche:null,
+    verified:false,
+    monetized:false,
+  })
+
+   const { listings } = useSelector(state => state.listing)
+
+   const filteredListings = listings.filter((listing)=>{
+       return true 
+   }) 
+
+
   return (
-    <div>
-      Marketplace
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+
+
+       <div className='flex items-center justify-between text-slate-500'>
+          <button onClick={()=>{navigate('/'); scrollTo(0,0)}}
+            className='flex items-center gap-2 py-5'>
+            <ArrowLeftIcon className='size-4'/>
+            Back to Home
+          </button>
+          <button onClick={()=>setShowFilterPhone(true)} className='flex sm:hidden items-center gap-2 py-5'>
+            <FilterIcon className='size-4'/>
+            Filters
+          </button>
+       </div>
+
+
+       <div className=' relative flex items-start justify-between  gap-8 pb-8'>
+             <FilterSidebar setFilters={setFilters} filters={filters} setShowFilterPhone={setShowFilterPhone} showFilterPhone={showFilterPhone}/>
+              <div className='flex-1 grid xl:grid-cols-2 gap-4'>
+                {filteredListings.sort((a,b)=>a.featured ? -1 : b.featured ? 1 : 0).map((listing,index)=>(
+                  <ListingCard listing={listing} key={index}/>
+                ))}
+              </div>
+       </div>
     </div>
   )
 }
